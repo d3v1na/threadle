eea2ace4548c8eb3904041f973e13c592b2de5ad
Devina Bhatnagar <db133@snu.edu.in>
2023-08-22 21:14:11 +0530

    welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:threadle/welcome.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xffFFE4E4),
+        backgroundColor: const Color(0xffFFE4E4),
         body: Container(
           child: const Welcome(),
         ),
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
@@ -8,10 +9,45 @@ class Welcome extends StatelessWidget {
     return Center(
       child: Column(
         children: [
+          const SizedBox(height: 198),
+          //text
+          Text(
+            'Welcome to',
+            style: GoogleFonts.poppins(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: const Color(0xff4E4E4E),
+            ),
+          ),
+          const SizedBox(height: 12),
+          Text(
+            'Threadle.',
+            style: GoogleFonts.pacifico(
+              fontSize: 38,
+              fontWeight: FontWeight.w400,
+              color: const Color(0xff000000),
+              letterSpacing: 3,
+            ),
+          ),
           Image.asset(
             './assets/images/logo.png',
-            width: 250,
+            width: 240,
           ),
+          OutlinedButton(
+            onPressed: () {}, 
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color(0xff000000),
+              backgroundColor: const Color(0xffFFFFFF),
+            ),
+            child: const Text(
+              'Get Started',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Poppins',
+              ),
+              ),
+          )
         ],
       ),
     );
